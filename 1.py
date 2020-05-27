9239c55b78d13de7c561bab614238d3470bf11ca
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2020-05-27 15:37:59 +0000

    update 1.py

--- 1.py
+++ 1.py
@@ -1,16 +1,46 @@
-#print ('*')
+number = 10
+while number > 1:
+    print ("*"),
+    number = number - 1
+print ("") 
+number = 9
+while number > 1:
+    print ("*"),
+    number = number - 1
+print ("") 
+number = 8
+while number > 1:
+    print ("*"),
+    number = number - 1
+print ("") 
+number = 7
+while number > 1:
+    print ("*"),
+    number = number - 1
+print ("")
+number = 6
+while number > 1:
+    print ("*"),
+    number = number - 1
+print ("") 
 number = 5
 while number > 1:
     print ("*"),
     number = number - 1
 print ("")
+number = 4
+while number > 1:
+    print ("*"),
+    number = number - 1
+print ("") 
 number = 3
 while number > 1:
     print ("*"),
     number = number - 1
-print ("")
+print ("") 
 number = 2
 while number > 1:
     print ("*"),
     number = number - 1
 print ("")
+
