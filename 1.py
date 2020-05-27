0a91d006f1266f0449859f2c7ceb90467b56a1a6
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2020-05-27 18:56:44 +0530

    1.py
    Loop time

---
+++ 1.py
@@ -0,0 +1,16 @@
+#print ('*')
+number = 5
+while number > 1:
+    print ("*"),
+    number = number - 1
+print ("")
+number = 3
+while number > 1:
+    print ("*"),
+    number = number - 1
+print ("")
+number = 2
+while number > 1:
+    print ("*"),
+    number = number - 1
+print ("")
